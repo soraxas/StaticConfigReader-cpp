5c9a20251d4212476a27b0e115e5325f0277f0ac
Tin Lai <oscar@tinyiu.com>
2022-08-03 17:19:15 +1000

    move func to translation unit
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- test.cpp
+++ test.cpp
@@ -1,37 +1,36 @@
 
 
+#include "include/StaticYamlConfigReader.h"
 #include "soraxas_toolbox/print_utils.h"
-
-
-#include "include/SimpleYamlConfigReader.h"
 
 using namespace sxs;
 
+int main()
+{
+    using config = StaticConfigReader<CT_STR("/home/tin/git-repos/simpleConfigReader-cpp/test.yaml"), CT_STR("/home/"
+                                                                                                             "tin/"
+                                                                                                             "git-"
+                                                                                                             "repos/"
+                                                                                                             "simpleCon"
+                                                                                                             "figReader"
+                                                                                                             "-cpp/"
+                                                                                                             "second."
+                                                                                                             "yaml")>;
 
-int main() {
-
-    using config = StaticConfigReader<CT_STR("/home/tin/git-repos/simpleConfigReader-cpp/test.yaml"), CT_STR(
-            "/home/tin/git-repos/simpleConfigReader-cpp/second.yaml") >;
-
-    using config2 = StaticConfigReader<CT_STR("/home/tin/git-repos/simpleConfigReader-cpp/test.yaml") >;
-
+    using config2 = StaticConfigReader<CT_STR("/home/tin/git-repos/simpleConfigReader-cpp/test.yaml")>;
 
     println("here");
 
     println(config::get<CT_STR("yes2"), double>());
 
-
-//    println(config::get<CT_STR("yes2"), bool>());
-
-
+    //    println(config::get<CT_STR("yes2"), bool>());
 
     println(config::get<CT_STR("yes"), std::string>());
     println(config::get<CT_STR("yes"), std::string>());
     println(config::get<CT_STR("yes3"), std::string>());
     println(config::get<CT_STR("yes3"), bool>());
 
-//    println(config2::get<CT_STR("yes3"), bool>());
-
+    //    println(config2::get<CT_STR("yes3"), bool>());
 
     println("ok");
 }
